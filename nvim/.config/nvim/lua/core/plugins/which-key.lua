bb8c193dd509095b7d40377406c53cc7a800117f
Ojas Bhagavath <ojas.bhagavath@gmail.com>
2024-12-04 21:30:09 +0530

    Update which-key.lua

--- nvim/.config/nvim/lua/core/plugins/which-key.lua
+++ nvim/.config/nvim/lua/core/plugins/which-key.lua
@@ -1,18 +1,35 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
-	keys = {
-		{
-			"<leader>?",
-			function()
-				require("which-key").show({ global = false })
-			end,
-			desc = "Buffer Local Keymaps (which-key)",
-		},
-	},
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+        {
+            "<leader>?",
+            function()
+                require("which-key").show({ global = false })
+            end,
+            desc = "Buffer Local Keymaps (which-key)",
+        },
+    },
+    config = function()
+        require("which-key").add({
+            { "<leader><tab>", group = "tab", icon = "󰓩" },
+            { "<leader>b", group = "buffer", icon = "" },
+            { "<leader>c", group = "code", icon = "" },
+            { "<leader>e", group = "yazi", icon = { icon = "󰇥", color = "yellow" } },
+            { "<leader>f", group = "find/file", icon = "" },
+            { "<leader>g", group = "git", icon = { icon = "", color = "red" } },
+            { "<leader>l", group = "lsp", icon = "" },
+            { "<leader>o", group = "options/toggle", icon = "" },
+            { "<leader>q", group = "sessions", icon = { icon = "", color = "white" } },
+            { "<leader>r", group = "reload", icon = "󰑓" },
+            { "<leader>s", group = "split", icon = "" },
+            { "<leader>u", group = "undo/ui" },
+            { "<leader>v", group = "vimtex" },
+            { "<leader>x", group = "diagnostics", icon = { icon = "", color = "yellow" } },
+            { "<leader>?", group = "which-key", icon = "" },
+            { "<leader>\\", group = "toggleterm", icon = { icon = "", color = "azure" } },
+            { "<localleader>l", group = "vimtex", icon = { icon = "", color = "green" } },
+        })
+    end,
 }
